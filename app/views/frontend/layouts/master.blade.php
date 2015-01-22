62ae7eb0cc97f0c95bcefea97f28157e68e837b0
Josue Vargas <jos.vargasv@gmail.com>
2015-01-22 00:49:27 -0300

    Agregado id analytics

--- app/views/frontend/layouts/master.blade.php
+++ app/views/frontend/layouts/master.blade.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="utf-8">
     @section('seo')
-        <title>'Boris Vargas'</title>
+        <title>Boris Vargas - Graphic Design</title>
         <meta name="description" content="I'm Boris Vargas. I'm a Graphic Designer currently based in Buenos Aires, Argentina. I work internationally as a full-time freelancer and always open for new projects and opportunities. I also sell high-end, ready to print Indesign templates to agencies and designers.">
     @show
     <meta name="viewport" content="width=device-width">
@@ -42,20 +42,14 @@
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
-            (function (b, o, i, l, e, r) {
-                b.GoogleAnalyticsObject = l;
-                b[l] || (b[l] =
-                        function () {
-                            (b[l].q = b[l].q || []).push(arguments)
-                        });
-                b[l].l = +new Date;
-                e = o.createElement(i);
-                r = o.getElementsByTagName(i)[0];
-                e.src = '//www.google-analytics.com/analytics.js';
-                r.parentNode.insertBefore(e, r)
-            }(window, document, 'script', 'ga'));
-            ga('create', 'UA-XXXXX-X');
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-58834830-1', 'auto');
             ga('send', 'pageview');
+
         </script>
 
         {{--<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>--}}
